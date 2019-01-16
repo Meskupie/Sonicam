f7d7e36e910fac43cf7845bf5dde60035e224737
Nicholas Skupien <nicholasskupien@gmail.com>
2019-01-16 13:34:07 -0500

    ejected app and added first button

--- web-ui/src/App.js
+++ web-ui/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className={classes.App}>
-        {/* <Layout/> */}
+        <Layout/>
         <People/>
       </div>
     );
--- web-ui/src/components/UI/Buttons/EqualizerButton/EqualizerButton.js
+++ web-ui/src/components/UI/Buttons/EqualizerButton/EqualizerButton.js
@@ -1,20 +1,20 @@
 import React from 'react';
-// import classes from './EqualizerButton.module.scss';
-import LogoSVG from './equalizergraphic.inline.svg';
+import classes from './EqualizerButton.module.scss';
+import Icon from './equalizergraphic.svg';
 
-const equalizerButton = () => <LogoSVG/>;
+//const equalizerButton = () => <Icon/>;
 
-// const equalizerButton = (props) => {
-//     let equalizerClass = [classes.Equalizer];
+const equalizerButton = (props) => {
+    let equalizerClass = [classes.Equalizer];
 
-//     //later add switch statement to determine how to display the image
-//     //and set class accordingly
+    //later add switch statement to determine how to display the image
+    //and set class accordingly
 
-//     return (
-//         <div className={equalizerClass.join(' ')}>
-//             <LogoSVG></LogoSVG>
-//         </div>
-//     );
-// }
+    return (
+        <div className={equalizerClass.join(' ')}>
+            <Icon className={classes.Icon}/>
+        </div>
+    );
+}
 
 export default equalizerButton;
